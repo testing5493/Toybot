dc5a8b8ee0902255b7133102d9b8bed2f7486502
Bitslayn <bitslayn@outlook.com>
2023-06-12 17:29:16 -0400

    Undo

--- src/main/java/com/jagrosh/vortex/logging/BasicLogger.java
+++ src/main/java/com/jagrosh/vortex/logging/BasicLogger.java
@@ -234,8 +234,8 @@ public class BasicLogger
             .setColor(Color.YELLOW)
                 .setAuthor(author==null ? getLoggingName(oldMessage) : getLoggingName(guild, author), author == null ? null : author.getEffectiveAvatarUrl())*/
     }
-    //Commented to prevent #0000 spam in modlogs
-    /*public void logNameChange(UserUpdateNameEvent event)
+
+    public void logNameChange(UserUpdateNameEvent event)
     {
         OffsetDateTime now = OffsetDateTime.now();
         User user = event.getUser();
@@ -287,7 +287,7 @@ public class BasicLogger
                         .setTimestamp(now)
                 );
             });
-    }*/
+    }
 
     public void logGuildJoin(GuildMemberJoinEvent event, OffsetDateTime now)
     {
